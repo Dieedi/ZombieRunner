05c65db8d8975e129f628acb6bee5d87ebad115e
Jeremy 'Dieedi' Pouillot <pouillot.j@free.fr>
2017-08-02 23:35:57 +0200

    FlareStick prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     
     public bool respawn;
+    public GameObject FlareStick;
 
     private GameObject[] SpawnPoints;
+    private GameObject UserFlareStick;
+    private GameObject Ground;
+    private bool haveFlare = false;
+    private int flareStickAvailable = 10;
 
 	// Use this for initialization
 	void Start () {
         SpawnPoints = GameObject.FindGameObjectsWithTag ("SpawnPoint");
         Respawn ();
+
+        Ground = GameObject.Find ("Terrain");
 	}
 	
 	// Update is called once per frame
@@ -19,11 +26,30 @@ public class Player : MonoBehaviour {
         if (respawn) {
             Respawn ();
         }
+
+        if (!haveFlare && Input.GetKeyDown (KeyCode.F) && flareStickAvailable > 0) {
+            LightFlare ();
+        } else if (Input.GetKeyDown (KeyCode.F) && flareStickAvailable > 0) {
+            DropLightFlare ();
+        } else if (flareStickAvailable == 0) {
+            Debug.LogWarning ("TODO : no-flare sound");
+        }
 	}
 
     void Respawn () {
         int spawnNumber = Mathf.Abs (Random.Range (0, 3));
         transform.position = SpawnPoints [spawnNumber].transform.position;
         respawn = false;
+    }
+
+    void LightFlare () {
+        UserFlareStick = Instantiate (FlareStick, transform.position, Quaternion.identity, transform);
+        haveFlare = true;
+    }
+
+    void DropLightFlare () {
+        UserFlareStick.transform.parent = Ground.transform;
+        UserFlareStick.GetComponent<Rigidbody> ().isKinematic = false;
+        haveFlare = false;
     }
 }
