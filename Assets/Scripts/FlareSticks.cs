ae31ecb2dbd2db1961fdc3f976920ab2a5296b1b
Jeremy 'Dieedi' Pouillot <pouillot.j@free.fr>
2017-08-04 01:05:16 +0200

    FlareStick count + pillar & heli/player position + pillard path ok

---
+++ Assets/Scripts/FlareSticks.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlareSticks : MonoBehaviour {
+
+    public int flareStickAvailable = 10;
+    public GameObject FlareStick;
+
+    private GameObject UserFlareStick;
+    private GameObject Ground;
+    private bool haveFlare = false;
+    private Text Count;
+
+	// Use this for initialization
+	void Start () {
+        Count = GetComponent<Text> ();
+        Count.text = flareStickAvailable.ToString ();
+        Ground = GameObject.Find ("Terrain");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!haveFlare && Input.GetKeyDown (KeyCode.F) && flareStickAvailable > 0) {
+            LightFlare ();
+        } else if (Input.GetKeyDown (KeyCode.F) && flareStickAvailable > 0) {
+            DropLightFlare ();
+        } else if (flareStickAvailable == 0) {
+            Debug.LogWarning ("TODO : no-flare sound");
+        }
+	}
+
+    void UpdateFlareCount () {
+        flareStickAvailable--;
+        Count.text = flareStickAvailable.ToString ();
+    }
+
+    void LightFlare () {
+        UserFlareStick = Instantiate (FlareStick, transform.position, Quaternion.identity, transform);
+        UpdateFlareCount ();
+        haveFlare = true;
+    }
+
+    void DropLightFlare () {
+        UserFlareStick.transform.parent = Ground.transform;
+        UserFlareStick.GetComponent<Rigidbody> ().isKinematic = false;
+        haveFlare = false;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     
     public bool respawn;
-    public GameObject FlareStick;
 
     private GameObject[] SpawnPoints;
-    private GameObject UserFlareStick;
-    private GameObject Ground;
-    private bool haveFlare = false;
-    private int flareStickAvailable = 10;
 
 	// Use this for initialization
 	void Start () {
         SpawnPoints = GameObject.FindGameObjectsWithTag ("SpawnPoint");
-        Respawn ();
-
-        Ground = GameObject.Find ("Terrain");
+        //Respawn ();
 	}
 	
 	// Update is called once per frame
@@ -26,30 +19,11 @@ public class Player : MonoBehaviour {
         if (respawn) {
             Respawn ();
         }
-
-        if (!haveFlare && Input.GetKeyDown (KeyCode.F) && flareStickAvailable > 0) {
-            LightFlare ();
-        } else if (Input.GetKeyDown (KeyCode.F) && flareStickAvailable > 0) {
-            DropLightFlare ();
-        } else if (flareStickAvailable == 0) {
-            Debug.LogWarning ("TODO : no-flare sound");
-        }
 	}
 
     void Respawn () {
         int spawnNumber = Mathf.Abs (Random.Range (0, 3));
         transform.position = SpawnPoints [spawnNumber].transform.position;
         respawn = false;
-    }
-
-    void LightFlare () {
-        UserFlareStick = Instantiate (FlareStick, transform.position, Quaternion.identity, transform);
-        haveFlare = true;
-    }
-
-    void DropLightFlare () {
-        UserFlareStick.transform.parent = Ground.transform;
-        UserFlareStick.GetComponent<Rigidbody> ().isKinematic = false;
-        haveFlare = false;
     }
 }
